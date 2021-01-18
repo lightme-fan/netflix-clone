e4926552b98ef2fed277bfcc95a5dddbb4231033
Fanilo Andriamarohaja <fanilo.and@onja.org>
2021-01-18 22:01:08 +0300

    Still updating...

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import JumbotronContainer from '../containers/jumbotron'
+
+function Home() {
+    return (
+        <JumbotronContainer></JumbotronContainer>
+    )
+}
+
+export default Home
